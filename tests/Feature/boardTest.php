10170694a3f5c31af6bebdd46e1bfa73c788fa94
Carter Stevens <carter@forthwithlife.co.uk>
2025-06-06 14:17:56 +0100

    Create test file for boards and write out tests that need completing

---
+++ tests/Feature/boardTest.php
@@ -0,0 +1,21 @@
+<?php
+
+// it('can create a board and assign it to a user', function () {
+
+// });
+
+// it('can delete a board assigned to a user', function () {
+// });
+
+// it('can edit a board assigned to a user', function () {
+// });
+
+// it('can edit a board name', function () {
+// });
+
+// it('can share a board with another user', function () {
+// });
+
+// it('can add columns and rows to a board', function () {
+
+// });
