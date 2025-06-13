12586dad4b016584aede357ef4a3af2a3c867844
Carter Stevens <carter@forthwithlife.co.uk>
2025-06-13 09:56:24 +0100

    Hide boards unless logged in

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
-Route::view('/my-boards/movies', 'board')->name('board');
-Route::view('/my-boards/', 'boards')->name('boards');
+Route::view('/my-boards/movies', 'board')->middleware('auth')->name('board');
+Route::view('/my-boards/', 'boards')->middleware('auth')->name('boards');
 
 
 Route::middleware('guest')->group(function () {
