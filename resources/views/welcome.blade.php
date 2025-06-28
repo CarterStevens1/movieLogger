cc72befd3bfc4f1cafcfdcb66d1536aef53d62a1
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2025-06-28 15:40:45 +0100

    Update home button and bg glow colors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
             <div class="relative isolate px-6 pt-14 lg:px-8">
                 <div class="fixed inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                     aria-hidden="true">
-                    <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+                    <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-green-500 to-secondaryGreen-200 opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
                         style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
                     </div>
                 </div>
@@ -35,13 +35,13 @@
                             excel but on rails for logging.</p>
                         <div class="mt-10 flex items-center justify-center gap-x-6">
                             <a href="{{ url('/register') }}"
-                                class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
+                                class="rounded-lg bg-peach-red-600 px-4 py-2 font-semibold text-white shadow-xs hover:bg-transparent border border-peach-red-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-peach-red-600 transition-all duration-300 ease-in-out">Get
                                 started today</a>
                         </div>
                     </div>
                 </div>
                 <div class="fixed inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
-                    <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+                    <div class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-green-500 to-secondaryGreen-200 opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
                         style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
                     </div>
                 </div>
