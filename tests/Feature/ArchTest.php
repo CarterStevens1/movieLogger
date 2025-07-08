7a3c758e06eb5b4e8824af84466b27dcf07f902f
Carter Stevens <carter@forthwithlife.co.uk>
2025-07-08 13:51:17 +0100

    fix double semicolon

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Redirect;
 
+
 it('does not use debugging functions', function () {
     expect(['dd', 'dump', 'ray'])
         ->not->toBeUsed();
@@ -10,4 +11,4 @@ it('does not use debugging functions', function () {
 it('uses the redirect facade for redirecting', function () {
     expect(['back', 'redirect', 'to_route'])
         ->not->toBeUsedIn('App\\Http\\Controllers\\');
-})->group('debug');;
+})->group('debug');
