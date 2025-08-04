eeb6b830e4e454523c095c24f6a41cfb9c26fdc2
Carter Stevens <carter@forthwithlife.co.uk>
2025-08-04 14:11:39 +0100

    sizing

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
 
 
     <x-forms.form method="POST" action="/register" enctype="multipart/form-data">
-        <x-forms.input name="name" label="Name" />
-        <x-forms.input name="username" label="Username" />
-        <x-forms.input name="email" label="Email" type="email" />
-        <x-forms.input name="password" label="Password" type="password" />
-        <x-forms.input name="password_confirmation" label="Confirm Password" type="password" />
+        <x-forms.input name="name" label="Name *" />
+        <x-forms.input name="username" label="Username *" />
+        <x-forms.input name="email" label="Email *" type="email" />
+        <x-forms.input name="password" label="Password *" type="password" />
+        <x-forms.input name="password_confirmation" label="Confirm Password *" type="password" />
 
         <x-forms.divider />
 
